7544469939d8eb06933435bed08e799fd7c80fa6
Deejpotter <deejpotter@gmail.com>
2021-07-16 22:08:04 +1000

    Initialised create react app and a couple of things.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 
+import reactDom from 'react-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
